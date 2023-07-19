e357f449e2dbd0e4cc407514c9048177edd7cbcd
hyeonjuuu <janghyeonjuu@gmail.com>
2023-07-19 13:40:03 +0900

    delegation 완료

--- client/chapter/dom/09.delegation.js
+++ client/chapter/dom/09.delegation.js
@@ -9,7 +9,7 @@
 /* 노드를 사용한 위임 ------------------ */
 
 // ^부모인 container에 이벤트를 적용.
-const container = getNode('.container');
+// const container = getNode('.container');
 
 // function handleDelegation(e) {
 //   // console.log(this); //container
@@ -23,35 +23,27 @@ const container = getNode('.container');
 //   }
 // }
 
+const container = getNode('.container');
+// e : 이벤트 객체.
 function handleDelegation(e) {
+  // e.target : 가장 먼저 클릭한 대상이 target
+  // 이벤트 타겟이 li가 아닌 span button이 될 수 있음
   let target = e.target;
-  //타겟에 클래스를 담아와줘
-  let className = attr(target, 'class');
+  // 선택한 타겟의 가장 가까운 부모인 li찾기
+  let li = target.closest('li');
 
-  // let dataName = target.dataset.name;
-  let dataName = attr(target, 'data-name');
+  // 함수는 읽다가 li가 없으면 나머지 실행하지 않고 return해줘
+  // 정확하게 li인 경우에만 체크할 수 있다.
+  if (!li) return;
 
-  if (dataName === 'A') {
-    console.log('A버튼 클릭');
+  // 하지만 컨테이너를 클릭하게되면 가까운 부모 중 li가 없기 때문에 null이 됨.
+  // li.getAttribute('class') -> null.getAttribute('class')
+  let className = attr(li, 'class');
+  let dataName = attr(li, 'data-name');
+
+  if (className === 'home') {
+    console.log('홈 실행!');
   }
-
-  //누른 대상의 속성을 가져옴
-  // console.log(target.getAttribute('class'));
-
-  // if (target.getAttribute('class') === 'a') {
-  //   console.log('A 버튼 클릭!');
-  // }
-
-  // if (target.getAttribute('class') === 'b') {
-  //   console.log('B 버튼 클릭!');
-  // }
-
-  // if (target.getAttribute('class') === 'c') {
-  //   console.log('C 버튼 클릭!');
-  // }
-
-  // if (target.getAttribute('class') === 'd') {
-  //   console.log('D 버튼 클릭!');
-  // }
 }
+
 container.addEventListener('click', handleDelegation);
