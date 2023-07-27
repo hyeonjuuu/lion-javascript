608323e1e73f3c1e212efe58f9e500207c8434ed
hyeonjuuu <janghyeonjuu@gmail.com>
2023-07-27 17:19:19 +0900

    storage 학습

--- client/main.js
+++ client/main.js
@@ -1,4 +1,18 @@
-console.log('hello js!');
+import { getNode, getStorage, setStorage } from './lib/index.js';
 
-const a = 10;
-const b = 11;
+const textField = getNode('#textField');
+
+function handleTextField() {
+  const value = this.value;
+
+  setStorage('text', value);
+}
+
+function init() {
+  getStorage('text').then((res) => {
+    textField.value = res;
+  });
+}
+
+textField.addEventListener('input', handleTextField);
+window.addEventListener('DOMContentLoaded', init);
